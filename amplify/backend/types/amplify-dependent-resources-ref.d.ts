5c678a7a83c8469c3040e3e7aa031d93afc3b961
Carlos Ferreira <carlosyells@yahoo.com>
2024-02-11 16:23:31 +0000

    trying to get backend to work with front end after backend works but front end no longer builds when pushed on Amplify.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "algarvevisit": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "dtbbackend604adf3e": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
