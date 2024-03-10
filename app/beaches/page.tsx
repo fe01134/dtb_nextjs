0632af2d6da80f95786de3f346b139032444ab7d
Carlos Ferreira <carlosyells@yahoo.com>
2024-03-10 13:05:20 +0000

    working REST API with Activities getting list of area names using Typescript Objects

--- app/beaches/page.tsx
+++ app/beaches/page.tsx
@@ -1,42 +1,57 @@
+// beaches/page.tsx
+"use client";
 import React from 'react';
 import ClientPagination from '@/components/client-pagination';
 import Area from '../../model/activity';
+import Activity from '../../model/activity';
+import Place from '../../model/activity';
+
+
+function displayValue(value: string | number | boolean | object){
+  if (typeof value === "string") {
+console.log(`The value is a string: &quot;${value}&quot;`);
+} else if (typeof value === "number") {
+console.log(`The value is a number: ${value}`);
+} else if (typeof value === "boolean") {
+console.log(`The value is a boolean: ${value}`);
+} else if (typeof value === "object") {
+console.log(`The value is a object: ${value}`);
+} else {
+console.log(`The value is of unsupported type: ${typeof value}`);
+}
+}
+
 //import Data from '../../pages/api/data/carvoeiro.json'
   export default async function BeachesPage(props:any) {
     const resp = await fetch("http://localhost:3000/api/beaches");
     const data = await resp.json();
     console.debug("carvoeirodata.json");
-    console.debug(data);
+    console.debug(data.areas);
+
+    console.debug("TypeData")
+    console.debug(displayValue(data.areas))
 
     // Map received data to Product objects using the createProduct method
     //const activities = data.areas[0].activities;
-    const myactivityObjects = data.areas.map((myactivityData) =>
-    Area.createArea(myactivityData)
+    // Create Place objects
+    const myplaceObjects = data.areas.map((myplaceData) =>
+      Place.createPlace(myplaceData.area)
     );
+
+    //const myactivityObjects = AreaModel.createArea(data.areas);
 
     return (
       <>
         <span className="font-bold text-4xl">Beaches</span>
-        Beaches Page Data
-        {JSON.stringify(data)}
-        
         <h1>Beach Activities City</h1>
+     
       <div>
-      
-      {myactivityObjects.map((area) => (
-      <ul>        
-          {area.getPlaceNames().map((one_area) => 
-            
-            <li key={one_area}>
-              <strong>Area:</strong> {one_area}
-            </li>
-            )
-          }
-        </ul>
-      ))}
-
+        {myplaceObjects.map((place)=> ( 
+          place.getName())
+        )}
       </div>
 
+  
         <div className="flex items-center text-sm font-medium text-muted-foreground">sub heading</div>
         <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg">
         <ClientPagination></ClientPagination>
--- model/activity.ts
+++ model/activity.ts
@@ -1,46 +1,22 @@
-interface Activity {
-  activity: string;
-  type: string;
-  places: Place[];
-}
+class Place {
 
-interface Place {
-  name: string;
-  tags: string[];
-}
+  private name : string;
 
-interface Area {
-  name: string;
-  activities: Activity[];
-}
-  
-  class Area {
-    private data: Area;
-  
-    constructor(data: Area) {
-      this.data = data;
-    }
-  
-    getPlaceNames(): string[] {
-      return Object.keys(this.data);
-    }
-  
-    getAreaNames(placeName: string): string[] {
-      const cityData = this.data[placeName];
-      return cityData ? Object.keys(cityData.Areas) : [];
-    }
-  
-    getActivities(placeName: string, areaName: string): Activity[] {
-      const cityData = this.data[placeName];
-      const areaData = cityData ? cityData.Areas[areaName] : null;
-      return areaData ? areaData.Activities?.map((a) => a.Activity) || [] : [];
-    }
-
-    static createArea(data: Area): Area {
-      return new Area(data);
-    }
+  constructor(name: string) {
+    this.name = name;
 
   }
+
+  getName(): string {
+    return this.name;
+  }
   
-  export default Area;
-  
+  static createPlace(name: string): Place {
+    return new Place(name);
+  }
+}
+
+
+
+export default Place ;
+
